4557bd363e1cdafdbcd2d0774a3529b35569f7d5
Jimmy <jimmy-ge.wang@cn.abb.com>
2017-07-27 11:03:47 +0800

    [QP-nano] Remove QP-nano examples
    Signed-off-by: Jimmy <jimmy-ge.wang@cn.abb.com>

--- source/qpn/examples/arm-cm/blinky_ek-tm4c123gxl/qk/gnu/dbg/qepn.d
+++
@@ -1,4 +0,0 @@
-dbg/qepn.o: c:\qp_lab\qpn\source\qepn.c ../../qpn_conf.h \
- c:\qp_lab\qpn\ports\arm-cm\qk\gnu\qfn_port.h \
- c:\qp_lab\qpn\include\qepn.h c:\qp_lab\qpn\include\qfn.h \
- c:\qp_lab\qpn\include\qkn.h c:\qp_lab\qpn\include\qassert.h
--- source/qpn/examples/arm-cm/blinky_ek-tm4c123gxl/qk/gnu/dbg/qfn.d
+++
@@ -1,4 +0,0 @@
-dbg/qfn.o: c:\qp_lab\qpn\source\qfn.c ../../qpn_conf.h \
- c:\qp_lab\qpn\ports\arm-cm\qk\gnu\qfn_port.h \
- c:\qp_lab\qpn\include\qepn.h c:\qp_lab\qpn\include\qfn.h \
- c:\qp_lab\qpn\include\qkn.h c:\qp_lab\qpn\include\qassert.h
