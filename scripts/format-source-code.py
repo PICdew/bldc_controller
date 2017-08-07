934a3d816a197edaa34400bfdc441680f3e8b6a1
Jimmy <jimmy-ge.wang@cn.abb.com>
2017-08-07 13:20:51 +0800

    [clang-format] Add exception list to code format script
    Signed-off-by: Jimmy <jimmy-ge.wang@cn.abb.com>

--- scripts/format-source-code.py
+++ scripts/format-source-code.py
@@ -4,13 +4,25 @@
 import os
 import os.path
 
+# All the file path include following keywords will be not be formated
+exceptionList = ['lib', 'qpn', 'main.c']
+
 path = os.getcwd() + '/../'
 for root, dirs, files in os.walk(path):
     for name in files:
-        if (name.endswith(".h") or name.endswith(".c")):
-            localpath = root + '/' + name
-            print localpath
-            os.system(path + "/bin/clang-format -i %s -style=File -sort-includes=false" %(localpath))
+        needFormat = True
+        localpath = root + '/' + name
+        localpath = localpath.replace("scripts/../", "").replace("\\", "/")
 
-print ''
+        for exceptElement in exceptionList:
+            if (-1 != localpath.find(exceptElement)):
+                # print '[Except]' + localpath
+                needFormat = False
+                break
+
+        if ((name.endswith(".h") or name.endswith(".c")) and needFormat):
+            print '[Process]' + localpath
+            os.system(
+                path + "/bin/clang-format -i %s -style=File -sort-includes=false" % (localpath))
+
 raw_input("Press any key to continue...")
