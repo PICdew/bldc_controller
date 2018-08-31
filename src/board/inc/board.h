e4fc77ec2d3bcf1faebb18edc87e6fc8143b90cc
Jimmy <jimmy-ge.wang@cn.abb.com>
2018-08-31 16:46:58 +0800

    [Project] Create board related files
    Signed-off-by: Jimmy <jimmy-ge.wang@cn.abb.com>

---
+++ src/board/inc/board.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2018 Wang Ge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+/*******************************************************************************
+ * API
+ ******************************************************************************/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void BOARD_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
